971ad3469aff0b0af324ba0c58ed345609379e54
Kim Taeyeong <xodud7737@gmail.com>
2024-03-10 22:22:15 +0900

    Studying

---
+++ Ch1. 파이썬.py
@@ -0,0 +1,74 @@
+### 파이썬 ###
+
+### 네이밍 컨벤션
+# 각 단어는 밑줄(_)로 구분한다.
+# 일반적으로 모두 소문자로 표기하며 경우에 따라 시작문자를 대문자로 표기
+# ex) snake_case = 1
+
+
+### 리스트 컴프리헨션
+# : 기존 리스트를 기반으로 새로운 리스트를 만들어내는 구문
+
+# a = []
+# for n in range(1,10+1):
+#     if n%2 == 1:
+#         a.append(n*2)
+# [2,6,10,14,18]
+        
+# =====
+
+# f = [n*2 for n in range(1,10+1) if n%2 == 1] # [2,6,10,14,18]
+
+# 리스트 외에도 딕셔너리,튜플에서도 가능! but 각각의 성질에 유의!
+
+# a = {}
+# for key,value in original.items():
+#     a[key] = value
+
+# =====
+
+# a = {key:value for key,value in original.items()}
+
+
+
+### 제너레이터(Generator)
+# 그때 그때 숫자를 생성, 굳이 생성하여 보관하지 않아도 된다
+# ex) 숫자 1억개를 만들어내는 프로그램, 1억개를 만든 후 어딘간에 보관하는 것이 아니라 i생성 => i삭제 => i+1 생성 꼴
+# yield 구문으로 Generator 리턴, (return 대신 사용된다.), 제너레이터가 여기까지 실행 중이던 값을 내보낸다는 의미.
+
+# def generator():
+#     yield 1
+#     yield 'string'
+#     yield True
+
+# g = generator()
+
+# print(g) #<generator object generator at 0x00000120FD243AC8>
+# print(next(g)) #1
+# print(next(g)) #string
+# print(next(g)) #True
+
+
+### range
+# 제너레이터의 방식으로 활용대는 함수
+
+# print(list(range(5))) #[0, 1, 2, 3, 4]
+# print(range(5)) #range(0, 5)
+# print(type(range(5))) #<class 'range'>
+# for i in range(5):
+#     print(i,end=' ') #0 1 2 3 4
+
+
+### 제너레이터의 효율성
+# import sys
+
+# a = [n for n in range(10000)]
+# b = range(10000)
+# print(len(a)) #10000
+# print(len(b)) #10000
+# print(sys.getsizeof(a)) #87624
+# print(sys.getsizeof(b)) #48
+
+
+
+
