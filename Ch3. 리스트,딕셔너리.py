831043dfc8ef6d5da5d27a3bdd605a52524294e4
Kim Taeyeong <xodud7737@gmail.com>
2024-03-11 22:39:46 +0900

    Studying

--- Ch3. 리스트,딕셔너리.py
+++ Ch3. 리스트,딕셔너리.py
@@ -26,4 +26,52 @@
 
 
 ### 딕셔너리 
+# 키/값 구조
+# 내부적으로 해쉬 테이블(Hash Table)로 구현됨.
+# 해쉬 테이블은 입력,조회 모두 O(1)에 가능, 다양한 타입의 키 가능
+
+# 딕셔너리의 주요 연산 시간 복잡도
+# len(a) : 요소의 개수를 리턴, O(1)
+# a[key] : 키를 조회하여 값을 리턴, O(1)
+# a[key] = value  키/값을 삽입, O(1)
+# key in a : 딕셔너리에 키가 존재하는지 확인, O(1)
+# cf) 파이썬 3.7 이상부터 딕셔너리 입력 순서 유지 그 외에는 유지하지 않는다.
+
+# 딕셔너리 활용
+# try:
+#     print(a['key4'])
+# except KeyError:
+#     print('존재하지 않은 키')
+
+# for k,v in a.items():
+#     print(k,v)
+# key1 value1
+# key2 value2
+# key3 value3
+
+# del a['key1']
+# print(a) #{'key2' : 'value2', 'key3' : 'value3'}
+
+
+
+### 딕셔너리 모듈
+
+# defaultdict 객체
+# 존재하지 않은 키를 조회할 경우, 에러메시지를 출력하는 대신 디폴트 값을 기준으로 해당 키에 대한 딕셔너리 아이템 생성
+
+# import collections
+
+# a = collections.defaultdict(int)
+# a['A'] = 5
+# a['B'] = 4
+# print(a) #defaultdict(<class 'int'>, {'A': 5, 'B': 4}
+# a['C'] += 1
+# print(a) #defaultdict(<class 'int'>, {'A': 5, 'B': 4, 'C': 1})
+
+
+# # counter 객체
+# # 아이템에 대한 갯수를 계산해 딕셔너리로 리턴
+# a = [1,2,3,4,5,5,5,6,6,]
+# b = collections.Counter(a)
+# print(b) #Counter({5: 3, 6: 2, 1: 1, 2: 1, 3: 1, 4: 1})
 
