49ca2611f09dfbc9d7bc719cd429fef5c367b182
Kim Taeyeong <xodud7737@gmail.com>
2024-03-16 21:39:31 +0900

    Studying

--- Ch5 선형자료구조/2. 두 수의 합.py
+++ Ch5 선형자료구조/2. 두 수의 합.py
@@ -7,14 +7,14 @@
 # 출력:
 # [0,1]
 
-### 풀이1(브루트 포스 계산,O(n^2)), 비효율적 풀이
+### 풀이1(브루트 포스 계산,O(n^2)), 비효율적 풀이, 5284 밀리초
 def twoSum(self, nums,target): #nums는 문자열 배열
     for i in range(len(nums)):
         for j in range(i+1,len(nums)):
             if nums[i]+nums[j] == target:
                 return [i,j]
 
-### 풀이2(in을 이용한 탐색)
+### 풀이2(in을 이용한 탐색) ,864밀리초
 # target - n 이 리스트에 존재하는지 확인!
 
 def twoSum(self, nums,target):
@@ -28,6 +28,7 @@ def twoSum(self, nums,target):
         
 
 ### 풀이3(첫 번째 수를 뺀 결과 키 조회)
+# dictionary 기반 풀이라 매우 빠름, 48밀리초
 def twoSum(self, nums,target): #nums 는 숫자 저장한 리스트
     nums_map = {}
     for i,num in enumerate(nums):
@@ -35,3 +36,27 @@ def twoSum(self, nums,target): #nums 는 숫자 저장한 리스트
     for i,num in enumerate(nums):
         if target - num in nums and i!=nums_map[target-num]: #찾고자하는 target-num의 index가 num의 인댁스와 같지 않다면
             return[i,nums_map[target-num]] # 각각의 인덱스 리턴
+        
+
+### 풀이4(조회 구전 개선,풀이3과 메커니즘은 같음),44밀리초
+def twoSum(self,nums,target):
+    nums_map = {}
+    for i,num in enumerate(nums):
+        if target - num in nums_map:
+            return [nums_map[target-num],i]
+        nums_map[num] = i
+
+
+### 풀이5 : 투 포인터 이용 (nums가 정렬 되어있을 때만 사용 가능)
+# 두 합이 같을 때 동안 left -> 으로 , right는 <-로 이동하는 메커니즘
+
+def twoSum(self,nums,target):
+    left,right = 0, len(nums)-1
+    while not left == right:
+        if nums[left] + nums[right] < target:
+            left += 1
+        elif nums[left] + nums[right] > target:
+            right -= 1
+        else:
+            return [left,right]
+        
